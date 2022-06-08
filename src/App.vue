0ba0d44918838f16e528f3f75d2c4b1043a2dc24
jdbacero <jdbacero@gmail.com>
2022-06-09 07:45:25 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/random">Random Todo</router-link>
     <router-link to="/about">About</router-link>
+    <h1>To-do List</h1>
   </nav>
   <router-view/>
 </template>
