ccf14aa83e31f25e8a86dfdf836bd052483310f5
jdbacero <jdbacero@gmail.com>
2022-06-09 11:18:02 +0800

    About page text edited

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hi!</h1>
+    <p>This was done as a simple Vue SPA to showcase static and dynamic routing in Vue!</p>
+    <p>All "todo list" data is taken from <a href="https://jsonplaceholder.typicode.com">typicode</a>, specifically <a href="https://jsonplaceholder.typicode.com/todos">this</a>.</p>
   </div>
 </template>
